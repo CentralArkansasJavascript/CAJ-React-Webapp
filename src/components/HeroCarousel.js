829b094ec536d5614a365d8da9bb244abd5bb512
Caleb Trachte <cetrachte@hotmail.com>
2018-09-25 15:26:14 -0500

    change carousel image size

--- src/components/HeroCarousel.js
+++ src/components/HeroCarousel.js
@@ -6,21 +6,21 @@ class HeroCarousel extends Component {
     return (
       <Carousel>
         <Carousel.Item >
-          <img width={900} height={500} alt="javascript-code" src={require('../assets/codes.jpg')} />
+          <img width={'100%'} height={500} alt="javascript-code" src={require('../assets/codes.jpg')} />
           <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item >
-          <img width={900} height={500} alt="code-placeholder-2" src={require('../assets/code-instruction.jpg')} />
+          <img width={'100%'} height={500} alt="code-placeholder-2" src={require('../assets/code-instruction.jpg')} />
           <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item >
-          <img width={900} height={500} alt="code-placeholder-3" src={require('../assets/laptop-code.jpg')} />
+          <img width={'100%'} height={500} alt="code-placeholder-3" src={require('../assets/laptop-code.jpg')} />
           <Carousel.Caption>
             <h3>Third slide label</h3>
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>

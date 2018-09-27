593901aa3526e8e462609e679745779939b347de
Caleb Trachte <cetrachte@hotmail.com>
2018-09-27 15:01:48 -0500

    add firebase

--- src/App.js
+++ src/App.js
@@ -5,8 +5,26 @@ import LandingCarousel from './components/LandingCarousel';
 import LandingHero from './components/LandingHero';
 import LandingMap from './components/LandingMap';
 import AppFooter from './components/AppFooter';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAOAHjXa7eB3Dt5x6W8VrvBbCHhtKx4dMw",
+  authDomain: "caj-react-app.firebaseapp.com",
+  databaseURL: "https://caj-react-app.firebaseio.com",
+  projectId: "caj-react-app",
+  storageBucket: "caj-react-app.appspot.com",
+  messagingSenderId: "360425980835"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+        this.state = {
+        };
+  }
+  
   render() {
     return (
       <div className="App">

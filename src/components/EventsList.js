37f6f840201a4d6cd2073ba1229683624c9bb842
Caleb Trachte <cetrachte@hotmail.com>
2018-10-02 17:00:34 -0500

    add api call - not working

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -2,12 +2,56 @@ import React, { Component } from 'react';
 import { Jumbotron } from 'react-bootstrap';
 
 class EventsList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.meetup.com/javascript-conway/events?photo-host=public&page=20&sig_id=247671999&sig=21562be4333298a04ed26b54a186d1b224b7ecb1")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log(error);
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   render() {
-    return (
-      <Jumbotron>
-        <h2>Join us at the Innovation Hub for our next event!</h2>
-      </Jumbotron>
-    );
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.name}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      );
+    }
   }
 }
 

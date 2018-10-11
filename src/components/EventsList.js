178c022aba478d067749589c17a541f5f37d017e
Caleb Trachte <cetrachte@hotmail.com>
2018-10-11 15:28:20 -0500

    fix card styling, remove event image (low quality)

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -62,15 +62,18 @@ class EventsList extends Component {
   }
 
   convertDate (unixDate){
-    let date = new Date(unixDate*1000);
-    let dateString = `${date.getMonth()}/${date.getDate()}/${date.getFullYear()} at ${date.getHours()}:${date.getMinutes()}`;
+    // Convert milliseconds since since 00:00:00 UTC, Thursday, 1 January 1970 (the epoch in Unix speak)
+    let date = new Date(unixDate);
+
+    // display time in our new format
+    let dateString = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} at ${date.getHours()}:${date.getMinutes()}`;
     return dateString;
   }
 
   render() {
     const { error, isLoaded, meetups, videos } = this.state;
     if (error) {
-      return <div>Error: {error.message}</div>;
+      return <div><h2>Error: {error.message}</h2></div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
@@ -78,22 +81,24 @@ class EventsList extends Component {
         <Row>
           <Col>
             <h2> Coming Up: </h2>
+            <hr />
           { meetups.map(meetup => (
             <Card key={meetup.id}>
               <CardBody>
                 <CardTitle>{meetup.name}</CardTitle>
                 <CardSubtitle>Coming up on {this.convertDate(meetup.time)}</CardSubtitle>
               </CardBody>
-              <CardImg src={meetup.photo_url} alt={meetup.name} />
+              <iframe title="innovation-hub-map" style={{ height: '100%', width: '100%', border:0}} src="https://www.google.com/maps/embed/v1/place?q=Arkansas%20Regional%20Innovation%20Hub%2C%20East%20Broadway%20Street%2C%20North%20Little%20Rock%2C%20AR%2C%20USA&key=AIzaSyAwTLE8hr4J66ZjRA7WVGIKTCW_9nbSarg" allowFullScreen></iframe>
               <CardBody>
                 <CardText dangerouslySetInnerHTML={{__html: meetup.description}}></CardText>
-                <CardLink href={meetup.event_url} target="blank">Check it out on Meetup!</CardLink>
+                <CardLink href={meetup.event_url} target="blank">Learn more about this event and RSVP on Meetup!</CardLink>
               </CardBody>
             </Card>
           ))}
           </Col>
           <Col>
           <h2> Past Events: </h2>
+          <hr />
           { videos.map(video => (
             (video.id.videoId) ?
               <Card key={video.id.videoId}>

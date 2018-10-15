83043e3ad5baeb9df42904853205c4fc3d036187
Caleb Trachte <cetrachte@hotmail.com>
2018-10-15 16:36:21 -0500

    adjust styling and disqus config, registered for disqus shortname

--- src/components/Discussions.js
+++ src/components/Discussions.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import Disqus from 'disqus-react';
+import { Jumbotron } from 'reactstrap';
 // Alternatively, import specific members:
 // import { DiscussionEmbed, CommentCount } from 'disqus-react';
 
 class Discussions extends React.Component {
     render() {
-        const disqusShortname = 'example';
+        const disqusShortname = 'central-arkansas-javascript';
         const disqusConfig = {
-            url: 'https://en.wikipedia.org/wiki/URL',
+            url: 'https://caj-react-app.firebaseapp.com/',
             identifier: 123,
-            title: "Creative Title",
+            title: "Share your thoughts!",
         };
 
         return (
             <div className="article">
-                <h1>{disqusConfig.title}</h1>
-                <Disqus.CommentCount shortname={disqusShortname} config={disqusConfig}>
-                    Comments
-                </Disqus.CommentCount>
-                <p>Body</p>
-                <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+              <Jumbotron>
+                <h2>{disqusConfig.title}</h2>
+              </Jumbotron>
+              <Disqus.CommentCount shortname={disqusShortname} config={disqusConfig}>
+              </Disqus.CommentCount>
+              <Disqus.DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
             </div>
         );
     }
